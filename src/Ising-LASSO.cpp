50252bcf91bf5104691b3d5d774e4146e0377d17
YunyiShen <yshen99@wisc.edu>
2020-05-30 21:19:24 -0500

    several glm-LASSO

---
+++ src/Ising-LASSO.cpp
@@ -0,0 +1,12 @@
+// [[Rcpp::depends(RcppArmadillo)]]
+#include <RcppArmadillo.h> // to use sparse matrix
+#include <tgmath.h>
+using namespace Rcpp;
+static const double pi = 3.141592653589793238462643383280;
+
+// [[Rcpp::depends(RcppProgress)]]
+#include <progress.hpp>
+#include <progress_bar.hpp>
+
+
+
