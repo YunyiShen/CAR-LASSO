ad3781f74071e7b5e9b9133893556c1ecd07a790
YunyiShen <yshen99@wisc.edu>
2020-10-09 02:06:41 -0500

    minor fix

--- real_data/Human/visualization.R
+++ real_data/Human/visualization.R
@@ -7,12 +7,16 @@ CAR <- get_CAR_MB(A_beta*B_binary,Graph_binary*A_Graph)
 
 among_spp <- graph_from_adjacency_matrix(CAR$C,mode = "directed",weighted = T,diag = F)
 abs_graph <- graph_from_adjacency_matrix(abs(Graph_binary*A_Graph),mode = "undirected",weighted = T,diag = F)
+linear_reg_graph <- graph_from_adjacency_matrix(t(CAR$C),weighted = T, mode = "directed")
 
+col_pn <- c("pink","lightblue")
 l <-layout_with_fr(among_spp)#, repulserad=vcount(among_spp)^3,area=vcount(among_spp)^2.4)
-plot(abs_graph,edge.arrow.size=.1,
+plot(among_spp,edge.arrow.size=.1,
      vertex.label=colnames(comp_mat)[-ncol(comp_mat)],
-     vertex.size = (100* page_rank(among_spp)$vector),
+     vertex.size = (3* alpha_centrality(among_spp)),
      #vertex.label=1:35,
+     layout = layout_with_gem,
+     edge.color = col_pn[1+(sign(E(among_spp)$weight)+1)/2],
      edge.curved=0)
 
 
@@ -42,8 +46,12 @@ edge_abs_df$weight <- abs(edge_abs_df$weight)
 
 full_graph <- graph.data.frame(edge_df,vertices_df,directed=T)
 full_graph_abs <- graph.data.frame(edge_abs_df,vertices_df,directed=T)
+rev_edge_df <- edge_abs_df
+rev_edge_df$from <- edge_df$to
+rev_edge_df$to <- edge_df$from
+rev_graph <- graph.data.frame(rev_edge_df,vertices_df,directed=T)
 
-col_pn <- c("pink","lightblue")
+
 col_ER <- c("orange","darkgreen")
 shape_ER <- c("square","circle")
 
@@ -52,11 +60,11 @@ E(full_graph)$edge.color <- col_pn[(sign(E(full_graph)$weight)+1)/2+1]
 l <-layout_with_fr(full_graph)
 par(mar=c(0,0,0,0)+.1)
 set.seed(428)
-plot(full_graph,edge.arrow.size=.1,
+plot(full_graph,edge.arrow.size=.2,
      #vertex.label=colnames(comp_mat)[1:35],
      vertex.label=c(colnames(comp_mat)[-ncol(comp_mat)],colnames(Design_dummy)),
-     #vertex.shape = shape_ER[c(rep(2,(ncol(comp_mat)-1)),rep(1,ncol(Design_dummy)))],
-     vertex.size = 100*(page_rank(full_graph)$vector),
+     vertex.shape = shape_ER[c(rep(2,(ncol(comp_mat)-1)),rep(1,ncol(Design_dummy)))],
+     vertex.size = 2*(alpha_centrality(full_graph)),
      vertex.color = col_ER[c(rep(1,(ncol(comp_mat)-1)),rep(2,ncol(Design_dummy)))],
      layout = layout_with_gem,
      #layout = layout_in_circle,
