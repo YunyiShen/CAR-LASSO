0dbbed8a39b59dc710c6adf40d98fe9acbe9baab
YunyiShen <yshen99@wisc.edu>
2020-10-13 18:49:53 -0500

    delete tests

--- real_data/Human/arkiv/raw_seq.R
+++
@@ -1,8 +0,0 @@
-download_data <- function(sample_id,url,pathsave = "./raw_seq/"){
-  download.file(gsub("<id>",sample_id,url),paste0(pathsave,sample_id,".fna"))
-}
-
-url_api <- "https://api.mg-rast.org/download/<id>?file=100.1"
-
-all_repos <- read.csv("./real_data/Human/raw_data/mgp154_metadata_library.csv")
-all_ids <- as.character(all_repos$metagenome_id)
