8beb4601dc0d0339cb90afd94087ec41170c8af4
ClaudiaSL <solislemus@wisc.edu>
2020-12-12 19:53:36 -0600

    new plots beta, omega, scaling

--- tests/Formal/Accurancy/plot_beta_learning.R
+++ tests/Formal/Accurancy/plot_beta_learning.R
@@ -1,3 +1,4 @@
+#all_res <- list.files("results",full.names = T)
 all_beta_files <- all_res[grep("graph_beta",all_res)]
 all_beta <- lapply(all_beta_files,read.csv,row.names=1)
 all_beta <- Reduce(rbind,all_beta)
@@ -6,11 +7,12 @@ all_beta_k30 <- all_beta[!is.na(all_beta$k),]
 all_beta_k30 <- all_beta_k30[all_beta_k30$k==30,]
 
 all_beta_k30$beta.sparsity <- factor( 1-all_beta_k30$s,levels = c(0.8,0.5))
-all_beta_k30$p <- paste0("#.predictors:",all_beta_k30$p)
-all_beta_k30$mod <- paste0("model:",all_beta_k30$mod)
+all_beta_k30$p <- paste0(all_beta_k30$p, " predictors")
+all_beta_k30 <- within(all_beta_k30, p<-factor(p, levels=c("5 predictors", "10 predictors")))
+all_beta_k30$mod <- paste0("model ",all_beta_k30$mod)
 
-all_beta_bayes <- all_beta_k30[!is.na(all_beta_k30$TP_bayes) & all_beta_k30$mod!="model:6",c(1:11,20)]
-all_beta_multireg <- all_beta_k30[all_beta_k30$algo=="multireg" & all_beta_k30$mod!="model:6",c(1:7,16:19,20)]
+all_beta_bayes <- all_beta_k30[!is.na(all_beta_k30$TP_bayes) & all_beta_k30$mod!="model 6",c(1:11,20)]
+all_beta_multireg <- all_beta_k30[all_beta_k30$algo=="multireg" & all_beta_k30$mod!="model 6",c(1:7,16:19,20)]
 
 all_beta_bayes$Sensitivity <- all_beta_bayes$TP_bayes/(all_beta_bayes$TP_bayes+all_beta_bayes$FN_bayes)
 all_beta_bayes$Specificity <- all_beta_bayes$TN_bayes/(all_beta_bayes$TN_bayes+all_beta_bayes$FP_bayes)
@@ -27,7 +29,7 @@ colnames(all_beta_multireg)[8:11] <- c("TP","TN","FP","FN")
 
 beta_learning_k30 <- rbind(all_beta_bayes,all_beta_multireg)
 
-
+## Original plot:
 beta_MCC <- ggplot(data = beta_learning_k30,aes(x=algo,y = MCC)) + 
   geom_boxplot(aes(fill = beta.sparsity)) + 
   facet_grid(mod~p) + 
@@ -40,6 +42,22 @@ beta_MCC <- ggplot(data = beta_learning_k30,aes(x=algo,y = MCC)) +
 
 beta_MCC
 ggsave("./tests/Formal/Accurancy/Figs/MCC_k30_Asmallprior_beta.pdf",beta_MCC,width = 8,height = 5,unit = "in")
+
+## New plot:
+beta_learning_k30 <- within(beta_learning_k30, algo<-factor(algo, levels=c("CAR-LASSO", "CAR-ALASSO", "SRG-LASSO", "multireg")))
+beta_MCC <- ggplot(data = beta_learning_k30,aes(x=algo,y = MCC)) + 
+  geom_point(aes(color = beta.sparsity), alpha=0.1, size=1)+
+  geom_boxplot(aes(fill = beta.sparsity)) + 
+  facet_grid(p~mod) + 
+  ylab("MCC on B") + 
+  xlab("") + theme_bw() +
+  theme(legend.position="top") + 
+  theme(text = element_text(size=14), 
+        axis.text.x = element_text(angle=90,hjust = 1,vjust=1),
+        plot.margin = margin(.15, .15, .15, .15, "cm"))
+
+beta_MCC
+ggsave("Figs/MCC_k30_Asmallprior_beta2.pdf",beta_MCC,width = 10,height = 8,unit = "in")
 
 
 beta_Sensitivity <- ggplot(data = beta_learning_k30,aes(x=algo,y = Sensitivity)) + 
