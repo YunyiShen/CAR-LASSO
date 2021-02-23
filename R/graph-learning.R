4d80f053a6e6b273abe3ab59c7d5c5b2ea044a10
YunyiShen <yshen99@wisc.edu>
2021-02-23 11:18:37 -0600

    cont

--- R/graph-learning.R
+++ R/graph-learning.R
@@ -2,7 +2,20 @@
 
 
 horseshoe.carlasso_out(obj, Bbar=NULL, A = NULL, nu=3, V=NULL ){
-    
+    y <- obj$data$response
+    design <- obj$data$design
+    if(obj$settings$link = "identity"){
+        multireg_res <- CAR_multireg(y,design,nrow(obj$MCMC_output$beta),
+                                     Bbar, A, nu, V)
+    }
+
+    if(obj$settings$link = "probit"){
+        multireg_res <- Pobit_CAR_multireg(y,design,obj$settings$n_burn_in,obj$settings$n_iter,
+                                     obj$settings$thin_by, 
+                                     Bbar, A, nu, V)
+    }
+
+
 
 
 }
