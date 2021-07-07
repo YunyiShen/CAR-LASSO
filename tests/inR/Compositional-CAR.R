c38dbc2adb811c1e2fcb75078a6e7167ef6b7216
YunyiShen <yshen99@wisc.edu>
2021-07-06 20:26:03 -0500

    test collected non gaussian methods

--- tests/inR/Compositional-CAR.R
+++ tests/inR/Compositional-CAR.R
@@ -10,6 +10,8 @@ n = 200
 p = 5
 
 sourceCpp("./src/Multinomial-CAR-ALASSO.cpp")
+sourceCpp("./src/CAR-ALASSO-hir.cpp")
+sourceCpp("./src/CAR-LASSO-hir.cpp")
 source("./tests/Formal/Accurancy/Graph_generator.R")
 
 set.seed(42)
@@ -48,6 +50,24 @@ test <- Multinomial_CAR_ALASSO_Cpp(Y,  Design, n_iter = 5000,
                             r_Omega = rep(.01,.5*(k-1)*k),
                             delta_Omega = rep(1e-6,.5*(k-1)*k),
                             lambda_diag = rep(0,k),
+                            ns = 100,m = 10, emax = 64,
+                            progress = T)
+
+test <- CAR_ALASSO_hir_Cpp(Y,  Design, link = 2,n_iter = 5000, 
+                            n_burn_in = 1000, thin_by = 10, 
+                            r_beta = .01+0*beta, delta_beta = 1e-6 + 0 * beta,
+                            r_Omega = rep(.01,.5*(k-1)*k),
+                            delta_Omega = rep(1e-6,.5*(k-1)*k),
+                            lambda_diag = rep(0,k),
+                            ns = 100,m = 10, emax = 64,
+                            progress = T)
+
+
+test <- CAR_LASSO_hir_Cpp(Y,  Design, link = 2,n_iter = 5000, 
+                            n_burn_in = 1000, thin_by = 10, 
+                            r_beta = 1, delta_beta = .01,
+                            r_Omega = 1,
+                            delta_Omega = .01,
                             ns = 100,m = 10, emax = 64,
                             progress = T)
 
